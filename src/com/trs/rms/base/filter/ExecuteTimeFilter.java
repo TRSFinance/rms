98567300af363424d2dbe5f8ff96bf2a03acb676
zxh <zxhjiutian@163.com>
2016-09-14 17:08:23 +0800

    jsp响应时间过滤器

---
+++ src/com/trs/rms/base/filter/ExecuteTimeFilter.java
@@ -0,0 +1,41 @@
+package com.trs.rms.base.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 
+ * 该过滤器用统计执行一个jsp所耗的时间
+ *
+ */
+public class ExecuteTimeFilter implements Filter {
+	private static final Logger LOG = LogManager.getLogger(ExecuteTimeFilter.class);
+	public static final String START_TIME = "_start_time";
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		//获取当前时间
+		long time = System.currentTimeMillis();
+		//设置开始时间
+		request.setAttribute("_start_time", Long.valueOf(time));
+		chain.doFilter(request, response);
+		//计算执行时间
+		time = System.currentTimeMillis() - time;
+		//写入日志
+		LOG.debug("process in {} ms: {}", Long.valueOf(time), request.getRequestURI());
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+}
