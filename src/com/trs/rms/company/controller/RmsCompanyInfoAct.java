fc535fb72ddb768035bfbdd66d16b5ab022df9bf
Ant <Ant@Ant-PC>
2016-09-26 16:53:50 +0800

    qiye

---
+++ src/com/trs/rms/company/controller/RmsCompanyInfoAct.java
@@ -0,0 +1,20 @@
+package com.trs.rms.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rmsCompanyInfo")
+public class RmsCompanyInfoAct {
+	
+	//v_add
+	//add
+	//v_edit
+	//edit
+	//del
+	//query
+	
+	
+	
+
+}
